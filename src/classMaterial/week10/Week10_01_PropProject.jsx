6b7079a1c7a22413422239ff8b18cb2ad6408de3
SudhaRaniD <devarakondasudha7@gmail.com>
2022-03-12 11:05:02 +0000

    firstcommit

---
+++ src/classMaterial/week10/Week10_01_PropProject.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CMNavBar from '../_CMNavBar'
+
+function Week10_01_PropProject() {
+  return (
+    <div className='app-container'>
+        <CMNavBar/>
+        <div className='content'></div>
+        <Week10_01_PropProject myName = "Justin"/>
+        </div>
+  );
+}
+
+export default Week10_01_PropProject
---
+++ src/classMaterial/week9/Week9_01_FirstComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CMNavBar from '../_CMNavBar';
+
+function Week9_01_FirstComponent() {
+  return (
+    <div className="app-container">
+      <CMNavBar />
+      <div className="content">
+        <h1>Well Done we have created a React Component</h1>
+      </div>
+      ;
+    </div>
+  );
+}
+
+export default Week9_01_FirstComponent;
